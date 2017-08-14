35627f9a3d268c8da68d381b4d5915d2d3d9137d
mirgags <markmiraglia@gmail.com>
2017-08-13 21:15:16 -0700

    can't used imported template

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 if ('content' in document.createElement('template')) {
 
+  class SingleProduct extends HTMLElement {
+    constructor() {
+      super();
+    }
+  };
+  customElements.define('single-product', SingleProduct);
+
   const entries = [
     [
       "The 1st Title",
@@ -15,14 +22,18 @@ if ('content' in document.createElement('template')) {
     ]
   ];
 
-  let p = document.querySelector('#textentry');
-  let divs = p.content.querySelectorAll('.singleEntry > *');
-  for(i=0;i<entries.length;i++) {
-    divs[0].textContent = entries[i][0];
-    divs[1].textContent = entries[i][1];
+  let link = document.querySelector('link[rel="import"]');
+  link.addEventListener('load', function(e) {
+    let importedDoc = link.import;
+    let p = document.querySelector('#textentry');
+    let divs = p.content.querySelectorAll('single-product > *');
+    for(i=0;i<entries.length;i++) {
+      divs[0].textContent = entries[i][0];
+      divs[1].textContent = entries[i][1];
 
-    let aDiv = document.querySelector('#mainContent');
-    let clone = document.importNode(p.content, true);
-    aDiv.appendChild(clone);
-  };
+      let aDiv = document.querySelector('#mainContent');
+      let clone = document.importNode(p.content, true);
+      aDiv.appendChild(clone);
+    };
+  });
 };
